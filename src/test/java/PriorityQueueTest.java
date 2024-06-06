fa328f74172c91b932efd403ab9845a678009c48
bky373 <bo373@naver.com>
2024-06-06 14:04:33 +0900

    PriorityQueueTest - topKFrequent

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,5 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 public class PriorityQueueTest {
@@ -55,6 +61,53 @@ public class PriorityQueueTest {
         @Override
         public int compare(Task t1, Task t2) {
             return Integer.compare(t1.getPriority(), t2.getPriority());
+        }
+    }
+
+
+    @Test
+    void topKFrequent() {
+        int[] nums = new int[]{1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
+        for (Integer n : freqMap.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        int[] ans = IntStream.range(0, k)
+                               .map(i -> heap.poll())
+                               .toArray();
+        assertThat(ans).containsExactlyInAnyOrder(1, 2);
+    }
+
+    static class Counter {
+
+        private int num;
+        private int count;
+
+        public Counter(int num, int count) {
+            this.num = num;
+            this.count = count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    static class CounterComparator implements Comparator<Counter> {
+
+        @Override
+        public int compare(Counter c1, Counter c2) {
+            return Integer.compare(c1.getCount(), c2.getCount());
         }
     }
 }
